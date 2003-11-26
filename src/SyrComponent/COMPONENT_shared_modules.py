c3137db7152882583c3da280026b4e907f4540b7
nri <nri@opencascade.com>
2003-11-26 10:18:52 +0000

    NRI : Add SuperVisionTest module into shared_modules.

---
+++ src/SyrComponent/COMPONENT_shared_modules.py
@@ -0,0 +1,19 @@
+"""
+
+"""
+
+# force COMPONENT importation at interpretor initialization
+# see salome_shared_modules.py
+# (avoids incomplete import at run time)
+
+print "============== import SuperVisionTest ======================="
+
+import SuperVisionTest
+
+# this function is required
+
+def init_shared_modules():
+   """
+      This function initializes shared modules that need to be
+   """
+   pass
